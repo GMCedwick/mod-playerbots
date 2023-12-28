51ab0cc2a73b425bda66674c706af3a4ded2e66e
GMCedwick <151778856+GMCedwick@users.noreply.github.com>
2023-12-28 09:15:46 -0600

    Update AiFactory.h
    Formatting: Converted all tabs to spaces for consistent indentation.
    Naming Conventions: Standardized method names to PascalCase.
    Parameter Naming: Renamed facade to botAI for clarity.
    Comments and Documentation: Added a comment block at the beginning of the file. It's recommended to add more detailed comments in the implementation file.
    Enum Class: Changed enum to enum class for stronger type safety.
    Type Consistency: Changed uint8 to uint8_t for standard integer types.

--- src/AiFactory.h
+++ src/AiFactory.h
@@ -1,36 +1,41 @@
+// AiFactory.h
+
 /*
- * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license, you may redistribute it and/or modify it under version 2 of the License, or (at your option), any later version.
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license.
+ * You may redistribute it and/or modify it under version 2 of the License, or (at your option),
+ * any later version.
  */
 
 #ifndef _PLAYERBOT_AIFACTORY_H
 #define _PLAYERBOT_AIFACTORY_H
 
 #include "Common.h"
-
 #include <map>
+#include <string>
 
 class AiObjectContext;
 class Engine;
 class Player;
 class PlayerbotAI;
 
-enum BotRoles : uint8;
+enum class BotRoles : uint8_t;
 
 class AiFactory
 {
-    public:
-        static AiObjectContext* createAiObjectContext(Player* player, PlayerbotAI* botAI);
-	    static Engine* createCombatEngine(Player* player, PlayerbotAI* const facade, AiObjectContext* aiObjectContext);
-	    static Engine* createNonCombatEngine(Player* player, PlayerbotAI* const facade, AiObjectContext* aiObjectContext);
-        static Engine* createDeadEngine(Player* player, PlayerbotAI* const facade, AiObjectContext* aibjectContext);
-        static void AddDefaultNonCombatStrategies(Player* player, PlayerbotAI* const facade, Engine* nonCombatEngine);
-        static void AddDefaultDeadStrategies(Player* player, PlayerbotAI* const facade, Engine* deadEngine);
-        static void AddDefaultCombatStrategies(Player* player, PlayerbotAI* const facade, Engine* engine);
+public:
+    static AiObjectContext* CreateAiObjectContext(Player* player, PlayerbotAI* botAI);
+    static Engine* CreateCombatEngine(Player* player, PlayerbotAI* botAI, AiObjectContext* aiObjectContext);
+    static Engine* CreateNonCombatEngine(Player* player, PlayerbotAI* botAI, AiObjectContext* aiObjectContext);
+    static Engine* CreateDeadEngine(Player* player, PlayerbotAI* botAI, AiObjectContext* aiObjectContext);
 
-        static uint8 GetPlayerSpecTab(Player* player);
-        static std::map<uint8, uint32> GetPlayerSpecTabs(Player* player);
-        static BotRoles GetPlayerRoles(Player* player);
-        static std::string GetPlayerSpecName(Player* player);
+    static void AddDefaultNonCombatStrategies(Player* player, PlayerbotAI* botAI, Engine* nonCombatEngine);
+    static void AddDefaultDeadStrategies(Player* player, PlayerbotAI* botAI, Engine* deadEngine);
+    static void AddDefaultCombatStrategies(Player* player, PlayerbotAI* botAI, Engine* engine);
+
+    static uint8_t GetPlayerSpecTab(Player* player);
+    static std::map<uint8_t, uint32_t> GetPlayerSpecTabs(Player* player);
+    static BotRoles GetPlayerRoles(Player* player);
+    static std::string GetPlayerSpecName(Player* player);
 };
 
-#endif
+#endif // _PLAYERBOT_AIFACTORY_H
